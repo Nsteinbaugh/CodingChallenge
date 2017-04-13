dd06f1aca460a107bc8af98340c68f7b63bd9dd7
Nsteinbaugh <steinbaughn@gmail.com>
2017-04-12 21:15:43 -0500

    Simple for loop

---
+++ CodingChallenge/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace CodingChallenge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.displayButton = new System.Windows.Forms.Button();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // displayButton
+            // 
+            this.displayButton.Location = new System.Drawing.Point(133, 225);
+            this.displayButton.Name = "displayButton";
+            this.displayButton.Size = new System.Drawing.Size(86, 34);
+            this.displayButton.TabIndex = 1;
+            this.displayButton.Text = "Display";
+            this.displayButton.UseVisualStyleBackColor = true;
+            this.displayButton.Click += new System.EventHandler(this.displayButton_Click);
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(51, 35);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputTextBox.Size = new System.Drawing.Size(290, 168);
+            this.outputTextBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 291);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.displayButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button displayButton;
+        private System.Windows.Forms.TextBox outputTextBox;
+    }
+}
+
---
+++ CodingChallenge/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodingChallenge
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void displayButton_Click(object sender, EventArgs e)
+        {
+            //placeholder for the outputs
+            string output = "";
+            //for loop to print out each number or word
+            for(int i = 1; i <= 100; i++)
+            {
+                //if statements to check if the number is divisible by 3, 5, both, or neither. 
+                if(i % 15 == 0)
+                {
+                    output += "MSUBears\r\n";
+                }
+                else if(i % 5 == 0)
+                {
+                    output += "Bears\r\n";
+                }
+                else if(i % 3 == 0)
+                {
+                    output += "MSU ";
+                }
+                else
+                {
+                    output += i + " ";
+                }
+            }
+            //output to the text box
+            outputTextBox.Text = output;
+        }
+    }
+}
